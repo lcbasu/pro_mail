88737afc1934c703c631c268a35d18ec9d3ffa16
Lokesh Chandra Basu <lokesh.basu@gmail.com>
2018-04-22 22:32:51 +0530

    feat: Add seed users for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(
+  email: "a@b.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "b@c.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "c@d.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "d@e.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "e@f.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "f@g.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "h@i.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "j@k.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(
+  email: "l@m.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+  
+User.create!(
+  email: "n@o.com",
+  password:              "foobar",
+  password_confirmation: "foobar")
+  
